9987a1cf197b10c84e0f05b0c039c1d85c44cf6b
Steve Cho <swcho0182@gmail.com>
2019-11-26 20:53:33 -0800

    session id implemented

--- models/profileModel.js
+++ models/profileModel.js
@@ -1,39 +1,57 @@
 const profileDb = require('../Database/databaseConnection');
 
 exports.createUser = (user) => {
+    console.log("IN CREATE USER")
+    console.log(user)
     const sql =
         `
-            INSERT INTO 
-            USERS (ID, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL) 
-            VALUES (
-                 ${user.id},
+            CALL CREATE_USER(
                 '${user.password}',
                 '${user.fname}',
                 '${user.lname}',
-                '${user.email}')
-        `;
+                '${user.email}',
+                '',
+                '',
+                '',
+                ''
+            )
+        `
     return profileDb.execute(sql);
 }
 
-exports.registerUser = (data) => {
+exports.registerUser = (data, id) => {
+    // const sql = 
+    //     `
+    //         UPDATE USERS
+    //         SET 
+    //             PROFILE_IMAGE_URL = '${data.imgURL}',
+    //             COUNTRY = '${data.country}',
+    //             ABOUT = '${data.about}',
+    //             DATE_OF_BIRTH = '${data.birthdate}'
+    //         WHERE ID=${id}
+    // `;
     const sql = 
         `
-            UPDATE USERS
-            SET 
-                PROFILE_IMAGE_URL = '${data.imgURL}',
-                COUNTRY = '${data.country}',
-                ABOUT = '${data.about}',
-                DATE_OF_BIRTH = '${data.birthdate}'
-            WHERE ID=${data.id}
-    `;
+            CALL MODIFY_USER(
+                ${id},
+                '${data.fname}',
+                '${data.lname}',
+                '${data.email}',
+                '${data.imgURL}',
+                '${data.country}',
+                '${data.about}'
+            )
+        `
     return profileDb.execute(sql);
 }
 
 exports.login = (data) => {
     const sql = 
         `
-            SELECT * FROM USERS
-            WHERE EMAIL = '${data.email}'
+            CALL GET_USER_PER_EMAIL_PASSWORD(
+                '${data.email}',
+                '${data.password}'
+            )
         `
     return profileDb.execute(sql);
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const jwt = require('jsonwebtoken');
-//const profileController = require('../controllers/profileController');
 
-//Session token verification (still testing it to work)
-// verify = (req, res, next) => {
-//     const token = req.headers['session-token'];
-//     console.log(req.header);
-//     if (!token) return res.status(401).send('Access Denied, login first')
-    
-//     try {
-//         console.log(token)
-//         const verified = jwt.verify(token, 'secret_thing');
-//         req.user=verified;
-//         next();
-//     } catch (err) {
-//         res.status(400).send('Invalid Token')
-//     }
-// }
+//redirectLogin Middleware
+//if user is not logged in (!req.session.userId), all routes using this middleware will redirect user to login
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/notLoggedIn');
+        //res.render('loginPage', {layout: 'signUp',err: 'Login First'})
+    } else {
+        next();
+    }
+}
 
 /**Login and Sign Up routes**/
 router.get('/', authController.loginPage);
+router.get('/notLoggedIn', authController.notLoggedIn)
 router.post('/signUp', authController.signUp);
 router.post('/login', authController.login);
-router.get('/registerPage/:id', authController.registerPage);
+router.get('/registerPage', redirectLogin, authController.registerPage);
 router.post('/register', authController.register)
+router.get('/homePage', redirectLogin, authController.homePage)
 
 //router.get('/:id', profileController.serve);
 
