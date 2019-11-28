34404c22f40af37f41a8e705ae49d9a44630e69c
Jeffery Wasty <jeff.wasty@gmail.com>
2019-11-28 13:21:12 -0800

    Setup profile to read from local db.

--- Database/databaseConnection.js
+++ Database/databaseConnection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit:8,
-    multipleStatements:true,
-    host:'us-cdbr-iron-east-05.cleardb.net',
-    user:'bb708863be87b7',
-    database:'heroku_c00af6dccc11356',
-    password:'be1d8c43'
+  connectionLimit: 8,
+  multipleStatements: true,
+  host: 'localhost',
+  user: 'root',
+  database: 'local_db',
+  password: 'Gyhtgy123'
 });
 
 module.exports = pool.promise();
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,35 +5,22 @@ const commentModel = require('../models/commentModel.js');
 exports.serveProfile = (req, res) => {
   let userData;
   let id = req.params.id;
-  let numPost;
-
   let user = profileModel.findUser(id);
 
   user.then(data => {
-    console.log(data);
-
-    // userData = {
-    //   id: rows[0].ID,
-    //   fname: rows[0].FIRST_NAME,
-    //   lname: rows[0].LAST_NAME,
-    //   country: rows[0].COUNTRY,
-    //   about: rows[0].ABOUT,
-    //   imageUrl: rows[0].PROFILE_IMAGE_URL
-    // };
+    userData = {
+      ID: data[0][0].ID,
+      FIRST_NAME: data[0][0].FIRST_NAME,
+      LAST_NAME: data[0][0].LAST_NAME,
+      COUNTRY: data[0][0].COUNTRY,
+      ABOUT: data[0][0].ABOUT,
+      PROFILE_IMAGE_URL: data[0][0].PROFILE_IMAGE_URL
+    };
+    res.render('profile', {
+      pageTitle: 'User Profile',
+      userCSS: true,
+      header: true,
+      UserData: userData
+    });
   });
-
-  //   userData = {
-  //     ID: '123',
-  //     FIRST_NAME: 'Kevin',
-  //     LAST_NAME: 'Hudson',
-  //     COUNTRY: 'Canada',
-  //     ABOUT: 'Team lead | Part time singer | Full time dad',
-  //     PROFILE_IMAGE_URL: 'https://randomuser.me/api/portraits/med/men/1.jpg'
-  //   };
-  //   res.render('profile', {
-  //     pageTitle: 'User Profile',
-  //     userCSS: true,
-  //     header: true,
-  //     UserData: userData
-  //   });
 };
