173f25d671fee8b488fd9448af9e721fdaa16eec
Jeffery Wasty <jeff.wasty@gmail.com>
2019-11-28 21:49:09 -0800

    profile sidebar

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,4 @@
 const profileModel = require('../models/profileModel');
-const postModel = require('../models/postModel');
-const commentModel = require('../models/commentModel.js');
 
 exports.serveProfile = (req, res) => {
   let userData;
@@ -21,6 +19,30 @@ exports.serveProfile = (req, res) => {
       userCSS: true,
       header: true,
       UserData: userData
+    });
+  });
+};
+
+exports.profileAllPosts = (req, res) => {
+  let userData;
+  let id = req.params.id;
+  let user = profileModel.findUser(id);
+
+  user.then(data => {
+    userData = {
+      ID: data[0][0].ID,
+      FIRST_NAME: data[0][0].FIRST_NAME,
+      LAST_NAME: data[0][0].LAST_NAME,
+      PROFILE_IMAGE_URL: data[0][0].PROFILE_IMAGE_URL,
+      ABOUT: data[0][0].ABOUT
+    };
+    res.render('home', {
+      UserData: userData,
+      Posts: data,
+      userCSS: true,
+      header: true,
+      sideCSS: true,
+      userValidated: true
     });
   });
 };
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const profileController = require('../controllers/profileController');
 
 router.get('/homePage/:id', profileController.serveProfile);
+router.get('/profile/posts/:id', profileController.profileAllPosts);
 
 module.exports = router;
