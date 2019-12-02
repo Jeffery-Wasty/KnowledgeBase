623318fe48d0196eca238e8967eb58e271e888db
Jeffery Wasty <jeff.wasty@gmail.com>
2019-12-01 19:45:13 -0800

    Can like user, restrictions apply, can view posts

--- models/profileModel.js
+++ models/profileModel.js
@@ -1,8 +1,7 @@
 const profileDb = require('../Database/databaseConnection');
 
-exports.createUser = (user) => {
-    const sql =
-        `
+exports.createUser = user => {
+  const sql = `
             CALL CREATE_USER(
                 '${user.password}',
                 '${user.fname}',
@@ -13,13 +12,12 @@ exports.createUser = (user) => {
                 '',
                 ''
             )
-        `
-    return profileDb.execute(sql);
-}
+        `;
+  return profileDb.execute(sql);
+};
 
 exports.registerUser = (data, id) => {
-    const sql =
-        `
+  const sql = `
             CALL MODIFY_USER(
                 ${id},
                 NULL,
@@ -29,33 +27,49 @@ exports.registerUser = (data, id) => {
                 '${data.country}',
                 '${data.about}'
             )
-        `
-    return profileDb.execute(sql);
-}
+        `;
+  return profileDb.execute(sql);
+};
 
-exports.login = (data) => {
-    const sql =
-        `
+exports.login = data => {
+  const sql = `
             CALL GET_USER_PER_EMAIL_PASSWORD(
                 '${data.email}',
                 '${data.password}'
             )
-        `
-    return profileDb.execute(sql);
-}
+        `;
+  return profileDb.execute(sql);
+};
 
 exports.getUserProfile = user_id => {
-    const sql =
-        `SELECT * FROM knowledgebase.users where id = '${user_id}'
-        `
-    return profileDb.execute(sql);
-}
+  const sql = `SELECT * FROM knowledgebase.users where id = '${user_id}'
+        `;
+  return profileDb.execute(sql);
+};
 
 exports.findUser = id => {
-    //   let sql = `CALL GET_PUBLIC_USER_INFO('${id}')`;
-    //   return profileDb.execute(sql);
-  
-    let sql = 'Select * from USERS Where id = ' + id;
-    return profileDb.execute(sql);
-  };
-  
+  //   let sql = `CALL GET_PUBLIC_USER_INFO('${id}')`;
+  //   return profileDb.execute(sql);
+
+  let sql = 'Select * from USERS Where ID = ' + id;
+  return profileDb.execute(sql);
+};
+
+exports.fetchLikes = id => {
+  let sql = 'Select * from USER_LIKES Where LIKED_USER_ID = ' + id;
+  return profileDb.execute(sql);
+};
+
+exports.fetchAlreadLiked = (id, sessionId) => {
+  let sql =
+    'Select * from USER_LIKES Where USER_ID = ' +
+    sessionId +
+    ' and LIKED_USER_ID = ' +
+    id;
+  return profileDb.execute(sql);
+};
+
+exports.saveLikes = (id, sessionId) => {
+  let sql = 'CALL LIKE_USER(' + sessionId + ', ' + id + ')';
+  return profileDb.execute(sql);
+};
