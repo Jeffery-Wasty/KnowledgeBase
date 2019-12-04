cee130691b7107c9c1406f7f9336e944f7efc6f9
Boning Yang <sylvartore@gmail.com>
2019-12-04 11:28:56 -0800

    fix fix fix

--- utils/emailServices.js
+++ utils/emailServices.js
@@ -12,7 +12,7 @@ const sendEmail = (sendTo, senderName, callback) => {
     });
 
     const mailOptions = {
-        from: process.env.SMTP_SEND_ADDRESS,
+        from: "Knowledge Base<sylvartore@gmail.com>",
         to: sendTo,
         subject: 'You got an new Conversation from KnowledgeBase',
         text: `${senderName} has started an conversation with you`
